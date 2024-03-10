3168ff75df9130f765229f069f5a6f9537a48d2b
user-die <vadim.vadimov123@mail.ru>
2024-03-10 19:00:56 +0500

    preparing app

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+import axios from "axios";
+
+function App() {
+  const [token, setToken] = useState();
+
+  useEffect(() => {
+    console.log("qwe");
+  }, []);
+
+  return <div className="App">qwe</div>;
+}
+
+export default App;
